3cadbfc379d53edaa61fe79771209e75833ab091
Nicholas Adamou <nicholasadamouemail@gmail.com>
2018-09-06 07:37:17 -0500

    added  cargo functions

--- utils.sh
+++ utils.sh
@@ -1671,3 +1671,37 @@ go_install() {
     fi
 
 }
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+# cargo functions
+
+is_cargo_installed() {
+
+    if ! cmd_exists "cargo"; then
+        return 1
+    fi
+
+}
+
+cargo_install() {
+
+    local package="$1"
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    # Check if `cargo` is installed.
+
+    is_cargo_installed || return 1
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+    if [ ! -f "$HOME/.cargo/bin/$package" ]; then
+        execute \
+            "cargo install --quiet $package" \
+            "cargo install ($package)"
+    else
+        print_success "($package) is already installed"
+    fi
+
+}
