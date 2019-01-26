c178dafcf2cede6b8ff9643da66385715c43dc9b
Nicholas Adamou <nicholasadamouemail@gmail.com>
2019-01-25 21:49:05 -0600

    updated utilities

--- scripts/modules/fish/omf.sh
+++ scripts/modules/fish/omf.sh
@@ -3,9 +3,9 @@
 # shellcheck source=/dev/null
 
 if [ "$(uname -a)" == "Linux" ] && grep -qEi 'debian|buntu|kali' /etc/*release; then
-		source /dev/stdin <<<"$(curl -s "https://raw.githubusercontent.com/nicholasadamou/utilities/master/scripts/base/debian/base.sh")"
+	source /dev/stdin <<<"$(curl -s "https://raw.githubusercontent.com/nicholasadamou/utilities/master/scripts/base/debian/base.sh")"
 elif [ "$(uname -a)" == "Darwin" ]; then
-		 source /dev/stdin <<<"$(curl -s "https://raw.githubusercontent.com/nicholasadamou/utilities/master/scripts/base/darwin/base.sh")"
+	source /dev/stdin <<<"$(curl -s "https://raw.githubusercontent.com/nicholasadamou/utilities/master/scripts/base/darwin/base.sh")"
 fi
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
